79f11d35c0a3c96fc2386dbb21194d72e8b58cd0
ferran.gonzalez.garcia <ferran.gonzalez.garcia@estudiantat.upc.edu>
2022-12-04 08:16:25 +0100

    Day 4 solutions posted!

---
+++ day4/first.py
@@ -0,0 +1,22 @@
+#Solution made by github.com/ulidev. Day _ Problem _
+
+import os
+import numpy as np
+
+file = open("input.txt", 'r')
+data = file.readlines()
+
+N = len(data)
+
+accumulated_counter = 0
+for element in data:
+	first, second = element.split(",")
+	first_start, first_end = first.split("-")
+	second_start, second_end = second.split("-")
+	first_start = int(first_start)
+	first_end = int(first_end)
+	second_start = int(second_start)
+	second_end = int(second_end)
+	if (first_start <= second_start and second_end <= first_end) or (first_start >= second_start and second_end >= first_end): accumulated_counter += 1
+
+print("Fully contained:", accumulated_counter)
